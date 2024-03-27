2100fbe62ec09d180479931b0068c0fe4de4fe78
LisaWyers <134444159+LisaWyers@users.noreply.github.com>
2024-03-27 21:11:23 +0000

    adding updated app.js and app.js test

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,23 +3,27 @@ import { Link } from "react-router-dom";
 
 const NavBar = () => {
   return (
-  <div className="nav-bar">
-    <img className="logo" src="https://commandshifthq.slack.com/files/U05KS145GQ4/F06RDUQTVRA/screenshot_2024-03-26_at_09.08.41.png" alt="swapzRus icon" />
-    <ul className="navbar-links">
-      <li className="navbar-links-item">
-        <Link to="/">Account</Link>
-      </li>
-      <li className="navbar-links-item">
-        <Link to="/">About Us</Link>
-      </li>
-      <li className="navbar-links-item">
-        <Link to="/">Toys</Link>
-      </li>
-      <li className="navbar-links-item">
-        <Link to="/">Add Toys</Link>
-      </li>
-    </ul>
-  </div>
+    <div className="nav-bar">
+      <img
+        className="logo"
+        src="logo.png"
+        alt="swapzRus icon"
+      />
+      <ul className="navbar-links">
+        <li className="navbar-links-item">
+          <Link to="/">Account</Link>
+        </li>
+        <li className="navbar-links-item">
+          <Link to="/">About Us</Link>
+        </li>
+        <li className="navbar-links-item">
+          <Link to="/">Toys</Link>
+        </li>
+        <li className="navbar-links-item">
+          <Link to="/">Add Toys</Link>
+        </li>
+      </ul>
+    </div>
   );
 };
 
