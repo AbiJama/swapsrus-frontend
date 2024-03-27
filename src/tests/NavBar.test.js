8b108bc6090ea25c434dbba088239ddab0c4047e
LisaWyers <134444159+LisaWyers@users.noreply.github.com>
2024-03-27 20:04:15 +0000

    adding basic Nav Bar and basic test

---
+++ src/tests/NavBar.test.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import { MemoryRouter } from "react-router-dom";
+import NavBar from "../components/NavBar";
+
+describe("NavBar", () => {
+  it("renders correctly", () => {
+    const { asFragment } = render(
+      <MemoryRouter>
+        <NavBar />
+      </MemoryRouter>
+    );
+
+    expect(asFragment()).toMachSnapshot();
+  });
+});
