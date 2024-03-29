a4790b454da2a954bb19389d150d28198d187580
LisaWyers <134444159+LisaWyers@users.noreply.github.com>
2024-03-29 19:46:01 +0000

    adding NavBar.js

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,29 +2,23 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 const NavBar = () => {
-  return (
-    <div className="nav-bar">
-      <img
-        className="logo"
-        src="logo.png"
-        alt="swapzRus icon"
-      />
-      <ul className="navbar-links">
-        <li className="navbar-links-item">
-          <Link to="/">Account</Link>
-        </li>
-        <li className="navbar-links-item">
-          <Link to="/">About Us</Link>
-        </li>
-        <li className="navbar-links-item">
-          <Link to="/">Toys</Link>
-        </li>
-        <li className="navbar-links-item">
-          <Link to="/">Add Toys</Link>
-        </li>
-      </ul>
-    </div>
-  );
+  <div className="nav-bar">
+    <img className="logo" src="logo.png" alt="swapzRus icon" />
+    <ul className="navbar-links">
+      <li className="navbar-links-item">
+        <Link to="/">Account</Link>
+      </li>
+      <li className="navbar-links-item">
+        <Link to="/">About Us</Link>
+      </li>
+      <li className="navbar-links-item">
+        <Link to="/">Toys</Link>
+      </li>
+      <li className="navbar-links-item">
+        <Link to="/">Add Toys</Link>
+      </li>
+    </ul>
+  </div>
 };
 
 export default NavBar;
