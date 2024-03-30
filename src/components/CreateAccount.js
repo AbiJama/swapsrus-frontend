521d017d93721cb60571f535c2b81c75f6dd9486
Abi Jama <abi_jama88@hotmail.com>
2024-03-30 14:07:01 +0000

    decided not to use firebase auth

--- src/components/CreateAccount.js
+++ src/components/CreateAccount.js
@@ -18,10 +18,41 @@ const CreateAccount = () => {
 
     const [fields, setfields] = useState(initalState.fields);
     const [alert, setAlert] = useState(initialState.alert);
+    const navigate = useNavigate();
 
+    const handleSignUp = (event) => {
+        event.preventDefault();
+        setAlert({ message: "", isSuccess: false });
+        createUserWithEmailAndPassword(auth, fields.email, fields.password)
+          .then((userCredential) => {
+        
+            const { user } = userCredential;
+            console.log("Account Created");
+            setAlert({
+              message: "Your account has been created!",
+              isSuccess: true,
+            });
+           
+            navigate("/");
+          })
+          .catch((error) => {
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            console.log(errorCode + errorMessage);
+            setAlert({ message: errorMessage, isSuccess: false });
+            setFields(initialState.fields);
+          });
+      };
+
+    const handleFieldChange = (event) => {
+        setFields((prev) => {
+          return {
+            ...prev,
+            [event.target.name]: event.target.value,
+          };
+        });
+      };
     
-
-
 
 
 }
