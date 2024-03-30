443fa32f5511bbb636170c11ae83b4c9a632cb95
Abi Jama <abi_jama88@hotmail.com>
2024-03-30 14:20:13 +0000

    added testing for alert component

--- src/components/CreateAccount.js
+++ src/components/CreateAccount.js
@@ -1,7 +1,6 @@
-import { createUserWithEmailAndPassword } from "firebase/auth";
+
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { auth } from "../config/firebase";
 import Alert from "./Alert";
 
 const CreateAccount = () => {
