719ea4e038bf0c266c367f39064728d026c8dc22
LisaWyers <134444159+LisaWyers@users.noreply.github.com>
2024-04-01 18:07:41 +0100

    add toys test

---
+++ src/tests/AddToys.test.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import { MemoryRouter } from "react-router-dom";
+import AddToys from "../components/AddToys";
+
+describe("AddToys", () => {
+  it("renders correctly", () => {
+    const { asFragment } = render(
+      <MemoryRouter>
+        <AddToys />
+      </MemoryRouter>
+    );
+
+    expect(asFragment()).toMatchSnapshot();
+    const toyTitle = screen.getByText(/Toy Title/i);
+
+    expect(toyTitle).toBeInTheDocument();
+  });
+});
