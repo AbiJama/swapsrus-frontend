75751080be7eac0b59e96a6935696498c1a1c930
Thasnima Begum <thasnima@hotmail.co.uk>
2024-04-03 19:20:23 +0100

    login files removed when merging main and thas

--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 function AboutUs() {
-	<div>About Us</div>;
+ return <div>About Us</div>;
 }
+
 export default AboutUs;
--- src/components/App.js
+++ src/components/App.js
@@ -1,26 +1,26 @@
 import React from "react";
-import {
-  BrowserRouter, Routes,
-  Route,
-} from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import NavBar from "./NavBar";
 import Toys from "./Toys";
 import AddToys from "./AddToys";
 import AboutUs from "./AboutUs";
-import Profile from "./Profile"
+import Profile from "./Profile";
+import Login from "./Login";
 
 function App() {
   return (
     <div className="App">
+      <h2>SwapRUs</h2>
       <NavBar />
       <Routes>
-        <Route path="about-us" element={<AboutUs />} />
-        <Route path="add-toys" element={<AddToys />} />
-        <Route path="toys" element={<Toys />} />
+        <Route path="/" element={<AboutUs />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/add-toys" element={<AddToys />} />
+        <Route path="/toys" element={<Toys />} />
         <Route path="profile" element={<Profile />} />
       </Routes>
     </div>
   );
-};
+}
 
 export default App;
