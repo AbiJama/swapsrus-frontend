83adb4cd94caf59dbca8b8e001278aff4992809c
Thasnima Begum <thasnima@hotmail.co.uk>
2024-04-03 18:44:59 +0100

    soft play image changed

--- src/components/Toys.js
+++ src/components/Toys.js
@@ -3,17 +3,18 @@ import getToys from "../requests/getToys";
 import ToyCard from "./ToyCard";
 import "../styles/toys.css"
 
+
 function Toys() {
  const [toys, setToys] = useState([]);
 
  useEffect(() => {
   const toyFunction = async () => {
    const toyVariable = await getToys();
-   console.log(toyVariable);
    setToys(toyVariable);
   };
   toyFunction();
  }, []);
+
 
  return (
   <div className="toys-display">
