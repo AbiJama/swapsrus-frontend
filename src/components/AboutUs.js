03a4f3e26c97c45662c15e4f03fbf6803e7e0ea7
LisaWyers <134444159+LisaWyers@users.noreply.github.com>
2024-04-04 17:46:06 +0100

    adding about us with image and button

--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -1,10 +1,21 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
-const AboutUs = () => {
+const AboutUs = ({ Toys, addToys }) => {
   return (
-    <div>
+    <div className="about-us">
       <h1>About Us</h1>
       <p>We are a toy swapping website</p>
+      <Link to="/toys">
+        <button type="button" className="browse-toys-logo" onClick={Toys}>
+          <img src="browse-toys.png" alt="logo with browse toys text" />
+        </button>
+      </Link>
+      <Link to="/add-toys">
+        <button type="button" className="upload-toys-logo" onClick={addToys}>
+          <img src="upload-toys.png" alt="logo with upload toys text" />
+        </button>
+      </Link>
     </div>
   );
 };
