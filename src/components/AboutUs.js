423df8d071bd3fb2f78efd4a671e3c35f4a68b73
LisaWyers <134444159+LisaWyers@users.noreply.github.com>
2024-04-07 11:35:47 +0100

    adding about us

--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -2,25 +2,23 @@ import React from "react";
 import { Link } from "react-router-dom";
 import "../styles/about-us.css";
 
-const AboutUs = ({ Toys, addToys }) => {
+function AboutUs() {
   return (
     <div className="about-us">
       <h1>About Us</h1>
       <p>We are a toy swapping website</p>
-      <div className="button-container">
+      <div className="browse-logo-toys">
         <Link to="/toys">
-          <button type="button" className="browse-toys-logo" onClick={Toys}>
-            <img src="browse-toys.png" alt="logo with browse toys text" />
-          </button>
+          <img src="browse-toys.png" alt="logo with browse toys text" />
         </Link>
+      </div>
+      <div className="upload-toys-logo">
         <Link to="/add-toys">
-          <button type="button" className="upload-toys-logo" onClick={addToys}>
-            <img src="upload-toys.png" alt="logo with upload toys text" />
-          </button>
+          <img src="upload-toys.png" alt="logo with upload toys text" />
         </Link>
       </div>
     </div>
   );
-};
+}
 
 export default AboutUs;
