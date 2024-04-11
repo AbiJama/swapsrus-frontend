3bc5dd2ce5a3462883c31e1f8d9d61bb51aea118
LisaWyers <134444159+LisaWyers@users.noreply.github.com>
2024-04-11 13:36:03 +0100

    adding toys.js working functionality to search and clear

--- src/components/Toys.js
+++ src/components/Toys.js
@@ -8,34 +8,39 @@ import "../styles/toys.css";
 
 function Toys() {
   const [toys, setToys] = useState([]);
-
   const [filteredToys, setFilteredToys] = useState([]);
   const { search } = useLocation();
+
   const handleSetToys = (category) => {
     if (category === "all") {
-      // Reset filtered toys to show all toys
-      setFilteredToys([]);
+      // Show all toys
+      setFilteredToys([]); // Display all toys
     } else {
       // Filter toys based on category
       const filtered = toys.filter((toy) => toy.type === category);
       setFilteredToys(filtered);
     }
   };
-  console.log(toys);
 
   useEffect(() => {
-    const toyFunction = async () => {
-      const toyVariable = await getToys();
-      setToys(toyVariable);
+    const fetchToys = async () => {
+      try {
+        const toyData = await getToys();
+        setToys(toyData);
+        setFilteredToys(toyData); // Initially display all toys
+      } catch (error) {
+        console.error("Error fetching toys:", error);
+      }
     };
-    toyFunction();
+
+    fetchToys();
   }, []);
 
   useEffect(() => {
     axios
       .get(`http://localhost:4000/api/v1/Toys${search}`)
       .then(({ data }) => setToys(data))
-      .catch((err) => console.error(err));
+      .catch((err) => console.error("Error fetching toys:", err));
   }, [search]);
 
   return (
@@ -43,22 +48,20 @@ function Toys() {
       <h2>Browse Toys:</h2>
       <div className="toys-display">
         <SideBar handleSetToys={handleSetToys} />
-        {filteredToys.map((toy) => {
-          return (
-            <ToyCard
-              key={toy.id}
-              id={toy.id}
-              title={toy.title}
-              type={toy.type}
-              description={toy.description}
-              ageRange={toy.ageRange}
-              condition={toy.condition}
-              postcode={toy.postcode}
-              image={toy.image}
-              setToys={setToys}
-            />
-          );
-        })}{" "}
+        {filteredToys.map((toy) => (
+          <ToyCard
+            key={toy.id}
+            id={toy.id}
+            title={toy.title}
+            type={toy.type}
+            description={toy.description}
+            ageRange={toy.ageRange}
+            condition={toy.condition}
+            postcode={toy.postcode}
+            image={toy.image}
+            setToys={setToys}
+          />
+        ))}
       </div>
     </>
   );
