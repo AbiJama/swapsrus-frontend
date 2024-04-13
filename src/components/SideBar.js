274591ce8369f27939c4d5a66427a30bfe0889e6
LisaWyers <134444159+LisaWyers@users.noreply.github.com>
2024-04-13 11:39:43 +0100

    adding sidebar

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -2,12 +2,24 @@ import React, { useState } from "react";
 import "../styles/sidebar.css";
 
 function SideBar({ handleSetToys }) {
-  const [selectedCategory, setSelectedCategory] = useState("");
+  const [selectedCategory, setSelectedCategory] = useState("all");
+  const [selectedCondition, setSelectedCondition] = useState("all");
+  const [selectedAgeRange, setSelectedAgeRange] = useState("all");
   const [isOpen, setIsOpen] = useState(false);
 
-  const handleClick = (category) => {
+  const handleCategoryClick = (category) => {
     setSelectedCategory(category);
-    handleSetToys(category);
+    handleSetToys(category, selectedCondition, selectedAgeRange);
+  };
+
+  const handleConditionClick = (condition) => {
+    setSelectedCondition(condition);
+    handleSetToys(selectedCategory, condition, selectedAgeRange);
+  };
+
+  const handleAgeRangeClick = (ageRange) => {
+    setSelectedAgeRange(ageRange);
+    handleSetToys(selectedCategory, selectedCondition, ageRange);
   };
 
   const toggleSidebar = () => {
@@ -15,11 +27,11 @@ function SideBar({ handleSetToys }) {
   };
 
   const clearFilter = () => {
-    setSelectedCategory("");
-    handleSetToys("all");
+    setSelectedCategory("all");
+    setSelectedCondition("all");
+    setSelectedAgeRange("all");
+    handleSetToys("all", "all", "all");
   };
-
-  console.log(selectedCategory);
 
   return (
     <div className={`sidebar ${isOpen ? "open" : ""}`}>
@@ -31,86 +43,167 @@ function SideBar({ handleSetToys }) {
         <>
           <h1 className="sidebar-heading">Filter by type</h1>
           <ul className="sidebar-links">
-            <button
-              type="button"
-              className="sidebar-links-item"
-              onClick={() => handleClick("Books")}
-            >
-              Books
-            </button>
-            <button
-              type="button"
-              className="sidebar-links-item"
-              onClick={() => handleClick("Pre-school")}
-            >
-              Pre-school
-            </button>
-            <button
-              type="button"
-              className="sidebar-links-item"
-              onClick={() => handleClick("Indoor")}
-            >
-              Indoor
-            </button>
-            <button
-              type="button"
-              className={`sidebar-links-item ${selectedCategory === "Outdoor" ? "selected" : ""}`}
-              onClick={() => handleClick("Outdoor")}
-            >
-              Outdoor
-            </button>
-            <button
-              type="button"
-              className="sidebar-links-item"
-              onClick={clearFilter}
-            >
-              Clear Filter
-            </button>
+            <li>
+              <button
+                type="button"
+                className="sidebar-links-item"
+                onClick={() => handleCategoryClick("Books")}
+              >
+                Books
+              </button>
+            </li>
+            <li>
+              <button
+                type="button"
+                className="sidebar-links-item"
+                onClick={() => handleCategoryClick("Pre-school")}
+              >
+                Pre-school
+              </button>
+            </li>
+            <li>
+              <button
+                type="button"
+                className="sidebar-links-item"
+                onClick={() => handleCategoryClick("Indoor")}
+              >
+                Indoor
+              </button>
+            </li>
+            <li>
+              <button
+                type="button"
+                className={`sidebar-links-item ${selectedCategory === "Outdoor" ? "selected" : ""}`}
+                onClick={() => handleCategoryClick("Outdoor")}
+              >
+                Outdoor
+              </button>
+            </li>
+            <li>
+              <button
+                type="button"
+                className="sidebar-links-item"
+                onClick={clearFilter}
+              >
+                Clear Filter
+              </button>
+            </li>
           </ul>
+
           <h2 className="sidebar-heading">Filter by Condition</h2>
           <ul className="sidebar-links">
-          <button
-              type="button"
-              className="sidebar-links-item"
-              onClick={() => handleClick("Brand New")}
-            >
-              Brand New
-            </button>
-            <button
-              type="button"
-              className="sidebar-links-item"
-              onClick={() => handleClick("Like New")}
-            >
-              Like New
-            </button>
-            <button
-              type="button"
-              className="sidebar-links-item"
-              onClick={() => handleClick("Good")}
-            >
-              Good
-            </button>
-            <button
-              type="button"
-              className="sidebar-links-item"
-              onClick={() => handleClick("Used")}
-            >
-              Used
-            </button>
-            <button
-              type="button"
-              className="sidebar-links-item"
-              onClick={() => handleClick("Defected")}
-            >
-              Defected
-            </button>
-            <button
-              type="button"
-              className="sidebar-links-item"
-              onClick={clearFilter}
-            >
-              Clear Filter
-            </button>
+            <li>
+              <button
+                type="button"
+                className="sidebar-links-item"
+                onClick={() => handleConditionClick("Brand New")}
+              >
+                Brand New
+              </button>
+            </li>
+            <li>
+              <button
+                type="button"
+                className="sidebar-links-item"
+                onClick={() => handleConditionClick("Like New")}
+              >
+                Like New
+              </button>
+            </li>
+            <li>
+              <button
+                type="button"
+                className="sidebar-links-item"
+                onClick={() => handleConditionClick("Good")}
+              >
+                Good
+              </button>
+            </li>
+            <li>
+              <button
+                type="button"
+                className="sidebar-links-item"
+                onClick={() => handleConditionClick("Used")}
+              >
+                Used
+              </button>
+            </li>
+            <li>
+              <button
+                type="button"
+                className="sidebar-links-item"
+                onClick={() => handleConditionClick("Defected")}
+              >
+                Defected
+              </button>
+            </li>
+            <li>
+              <button
+                type="button"
+                className="sidebar-links-item"
+                onClick={clearFilter}
+              >
+                Clear Filter
+              </button>
+            </li>
+          </ul>
+
+          <h3 className="sidebar-heading">Filter by Age Range</h3>
+          <ul className="sidebar-links">
+            <li>
+              <button
+                type="button"
+                className="sidebar-links-item"
+                onClick={() => handleAgeRangeClick("0-3")}
+              >
+                0-3
+              </button>
+            </li>
+            <li>
+              <button
+                type="button"
+                className="sidebar-links-item"
+                onClick={() => handleAgeRangeClick("3-6")}
+              >
+                3-6
+              </button>
+            </li>
+            <li>
+              <button
+                type="button"
+                className="sidebar-links-item"
+                onClick={() => handleAgeRangeClick("6-9")}
+              >
+                6-9
+              </button>
+            </li>
+            <li>
+              <button
+                type="button"
+                className="sidebar-links-item"
+                onClick={() => handleAgeRangeClick("9-12")}
+              >
+                9-12
+              </button>
+            </li>
+            <li>
+              <button
+                type="button"
+                className="sidebar-links-item"
+                onClick={() => handleAgeRangeClick("12+")}
+              >
+                12+
+              </button>
+            </li>
+            <li>
+              <button
+                type="button"
+                className="sidebar-links-item"
+                onClick={clearFilter}
+              >
+                Clear Filter
+              </button>
+            </li>
           </ul>
         </>
       )}
