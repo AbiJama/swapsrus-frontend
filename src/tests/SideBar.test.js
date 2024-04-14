7cf155ffb578b02ae3fac69411905150c0ee6352
LisaWyers <134444159+LisaWyers@users.noreply.github.com>
2024-04-14 14:56:28 +0100

    adding sidebar test to test for condition and age range

--- src/tests/SideBar.test.js
+++ src/tests/SideBar.test.js
@@ -17,5 +17,18 @@ describe("SideBar", () => {
     expect(screen.getByText(/Indoor/i)).toBeInTheDocument();
     expect(screen.getByText(/Outdoor/i)).toBeInTheDocument();
     expect(screen.getByText(/Books/i)).toBeInTheDocument();
+
+    expect(screen.getByText(/Filter By Age Range/i)).toBeInTheDocument();
+    expect(screen.getByText(/0-3/i)).toBeInTheDocument();
+    expect(screen.getByText(/3-6/i)).toBeInTheDocument();
+    expect(screen.getByText(/6-9/i)).toBeInTheDocument();
+    expect(screen.getByText(/9-12/i)).toBeInTheDocument();
+
+    expect(screen.getByText(/Filter By Condition/i)).toBeInTheDocument();
+    expect(screen.getByText(/Brand New/i)).toBeInTheDocument();
+    expect(screen.getByText(/Like New/i)).toBeInTheDocument();
+    expect(screen.getByText(/Good/i)).toBeInTheDocument();
+    expect(screen.getByText(/Defected/i)).toBeInTheDocument();
+
   })
 });
