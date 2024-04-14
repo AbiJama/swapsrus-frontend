77348ffc9f34b5ecf61f2ac11b3d72aafb924a75
LisaWyers <134444159+LisaWyers@users.noreply.github.com>
2024-04-14 14:22:20 +0100

    updating form to not submit when empty and clear when submitted

--- src/components/AddToys.js
+++ src/components/AddToys.js
@@ -5,51 +5,56 @@ import "../styles/add-toys.css";
 
 function AddToys() {
   const initialState = {
-    fields: {
-      title: "",
-      type: "books",
-      condition: "new",
-      ageRange: "",
-      description: "",
-      borrowPeriod: "",
-      postcode: "",
-      image: "",
-    },
-    alert: {
-      message: "",
-      isSuccess: false,
-    },
+    title: "",
+    type: "books",
+    condition: "new",
+    ageRange: "",
+    description: "",
+    borrowPeriod: "select borrow period",
+    postcode: "",
+    image: "",
   };
 
-  const [fields, setFields] = useState(initialState.fields);
-  const [alert, setAlert] = useState(initialState.alert);
+  const [fields, setFields] = useState(initialState);
+  const [alert, setAlert] = useState({ message: "", isSuccess: false });
 
   const handleAddToys = async (event) => {
     event.preventDefault();
     setAlert({ message: "", isSuccess: false });
+
+   if (!fields.title || !fields.ageRange || !fields.description || !fields.borrowPeriod) {
+    setAlert({
+      message: "Please fill out all required fields.",
+      isSuccess: false,
+    });
+    return;
+  }
+
     try {
       const response = await axios.post("http://localhost:4000/toys", fields);
       setAlert({
         message: "The item has been added successfully!",
         isSuccess: true,
       });
+      setFields(initialState);
     } catch (error) {
       setAlert({
         message: "Server error, please try again later.",
         isSuccess: false,
       });
     }
-    setFields(fields);
   };
-
   const handleFieldChange = (event) => {
     const { name, value } = event.target;
-    setFields({ ...fields, [name]: value });
+    setFields((prevFields) => ({
+      ...prevFields,
+      [name]: value,
+    }));
   };
+
 
   return (
     <div className="add-toys">
-      <Alert message={alert.message} success={alert.isSuccess} />
       <h2>Add Toys</h2>
       <form className="add-toy-form" onSubmit={handleAddToys}>
         <label htmlFor="title">
@@ -80,7 +85,7 @@ function AddToys() {
             <option value="Indoor">Indoor</option>
             <option value="Outdoor">Outdoor</option>
           </select>
-        </label>
+          </label>
         <label htmlFor="condition">
           Condition:
           <br />
@@ -111,10 +116,10 @@ function AddToys() {
             onChange={handleFieldChange}
           >
             <option value="select age range">Select age range</option>
-            <option value="0-3">0-3</option>
-            <option value="3-6">3-6</option>
-            <option value="6-9">6-9</option>
-            <option value="9-12">9-12</option>
+            <option value="0-3">0-3 years</option>
+            <option value="3-6">3-6 years</option>
+            <option value="6-9">6-9 years</option>
+            <option value="9-12">9-12 years</option>
             <option value="12+">12+</option>
           </select>
         </label>
@@ -139,8 +144,8 @@ function AddToys() {
             id="borrowPeriod"
             name="borrowPeriod"
             type="number"
-            placeholder="0"
-            value={fields.price}
+            placeholder="select borrow period"
+            value={fields.borrowPeriod}
             onChange={handleFieldChange}
           >
             <option value="select borrow period">Select Borrow Period</option>
@@ -154,7 +159,6 @@ function AddToys() {
         <label htmlFor="postcode">
           Postcode:
           <br />
-          <br />
           <input
             type="text"
             name="postcode"
@@ -166,7 +170,6 @@ function AddToys() {
         </label>
         <label htmlFor="image">
           Image Upload:
-          <br />
           <br />
           <input
             type="text"
@@ -181,6 +184,7 @@ function AddToys() {
           Add
         </button>
       </form>
+      <Alert message={alert.message} success={alert.isSuccess} onChange={handleFieldChange}/>
     </div>
   );
 }
