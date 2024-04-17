d72a8bf159a4193f870abe8966a4889207e33f47
Abi Jama <abi_jama88@hotmail.com>
2024-04-17 22:26:22 +0100

    delete function

--- src/components/ToyCard.js
+++ src/components/ToyCard.js
@@ -5,82 +5,87 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faTrashCan, faEnvelope } from "@fortawesome/free-solid-svg-icons";
 
 export default function ToyCard(props) {
- const [heart, setHeart] = useState("white");
+  const [heart, setHeart] = useState("white");
 
- const {
-  id,
-  title,
-  description,
-  condition,
-  ageRange,
-  postcode,
-  type,
-  image,
-  setToys,
- } = props;
- const imageSource = `/images/${image}.png`;
+  const {
+    id,
+    title,
+    description,
+    condition,
+    ageRange,
+    postcode,
+    type,
+    image,
+    setToys,
+  } = props;
+  const imageSource = `/images/${image}.png`;
 
- const handleDelete = () => {
-  return axios
-   .delete(`http://localhost:4000/toys/${id}`)
-   .then(setToys((prevToys) => prevToys.filter((toy) => toy.id !== id)))
-   .catch((error) => console.log(error));
- };
+  const handleDelete = () => {
+    return axios
+      .delete(`http://localhost:4000/toys/${id}`)
+      .then(() =>
+        setToys((prevToys) => prevToys.filter((toy) => toy.id !== id)),
+      )
+      .catch((error) => console.log(error));
+  };
+  const handleContact = () => {};
 
- const handleContact = () => {};
+  const handleClick = () => {
+    if (heart === "white") {
+      setHeart("pink");
+    } else if (heart === "pink") {
+      setHeart("white");
+    }
+  };
 
- const handleClick = () => {
-  if (heart === "white") {
-   setHeart("pink");
-  } else if (heart === "pink") {
-   setHeart("white");
-  }
- };
-
- return (
-  <div className="toy-card">
-   <img className="toy-card_image" src={imageSource} alt="product" />
-   <h2>{title}</h2>
-   <p>{description}</p>
-   <p>Type:{type}</p>
-   <p>Age Range: {ageRange}</p>
-   <p>Condition: {condition}</p>
-   <p>Postcode: {postcode}</p>
-   <div className="toy-card_buttons">
-    <button
-     className="screen toy-card_button toy-card_delete"
-     type="button"
-     onClick={handleDelete}
-    >
-     Delete <FontAwesomeIcon icon={faTrashCan} />
-    </button>
-    <button
-     className="mobile toy-card_button toy-card_delete"
-     type="button"
-     onClick={handleDelete}
-     aria-label="delete-button"
-    >
-    <FontAwesomeIcon icon={faTrashCan} />
-    </button>
-    <button
-     className="screen toy-card_button toy-card_contact"
-     type="button"
-     onClick={handleContact}
-    >
-     Contact  <FontAwesomeIcon icon={faEnvelope} />
-    </button>
-    <button
-     className="mobile toy-card_button toy-card_contact"
-     type="button"
-     onClick={handleContact}
-     aria-label="contact-button"
-    >
-       <FontAwesomeIcon icon={faEnvelope} />
-    </button>
-    <button type="button" className="toy-card_button toy-card_heart" onClick={handleClick}>
-     <img src={`/images/heart_${heart}.png`} alt="favourite icon" />
-    </button>
-   </div>
-  </div>
- );
+  return (
+    <div className="toy-card">
+      <img className="toy-card_image" src={imageSource} alt="product" />
+      <h2>{title}</h2>
+      <p>{description}</p>
+      <p>Type:{type}</p>
+      <p>Age Range: {ageRange}</p>
+      <p>Condition: {condition}</p>
+      <p>Postcode: {postcode}</p>
+      <div className="toy-card_buttons">
+        <button
+          className="screen toy-card_button toy-card_delete"
+          type="button"
+          onClick={handleDelete}
+        >
+          Delete <FontAwesomeIcon icon={faTrashCan} />
+        </button>
+        <button
+          className="mobile toy-card_button toy-card_delete"
+          type="button"
+          onClick={handleDelete}
+          aria-label="delete-button"
+        >
+          <FontAwesomeIcon icon={faTrashCan} />
+        </button>
+        <button
+          className="screen toy-card_button toy-card_contact"
+          type="button"
+          onClick={handleContact}
+        >
+          Contact <FontAwesomeIcon icon={faEnvelope} />
+        </button>
+        <button
+          className="mobile toy-card_button toy-card_contact"
+          type="button"
+          onClick={handleContact}
+          aria-label="contact-button"
+        >
+          <FontAwesomeIcon icon={faEnvelope} />
+        </button>
+        <button
+          type="button"
+          className="toy-card_button toy-card_heart"
+          onClick={handleClick}
+        >
+          <img src={`/images/heart_${heart}.png`} alt="favourite icon" />
+        </button>
+      </div>
+    </div>
+  );
 }
